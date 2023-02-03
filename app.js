33a6d0eeab449020f82ed6e3b4eba2517b19bd7d
amer-tech <alicalan01@gmail.com>
2023-02-03 11:01:28 +0100

    Full working version without minmax algoritham

--- app.js
+++ app.js
@@ -6,13 +6,14 @@
   ];
 
   let playerTurn = 1;
-  let gameMode = "playerVplayer";
+  let gameOver = true;
+  let gameMode = "playerVsAi";
 
   const gridCells = document.querySelectorAll("[data-cell]");
   gridCells.forEach((gridCell, index) => {
     gridCell.addEventListener("click", () => {
       addMark(index);
-      checkWinner();
+      setTimeout(checkWinner, 200);
     });
   });
 
@@ -27,13 +28,29 @@
       gameBoard[row][col] = -1;
       playerTurn++;
       updateCell(index);
+      if (gameMode === "playerVsAi") setTimeout(aiPlay, 200);
       // check if player two should play
-    } else if (playerTurn % 2 === 0 && gameMode === "playerVplayer") {
+    } else if (playerTurn % 2 === 0 && gameMode === "playerVsPlayer") {
       // change gameBoard
       gameBoard[row][col] = 1;
       playerTurn++;
       updateCell(index);
     }
+  }
+
+  function aiPlay() {
+    if (playerTurn === 10) return;
+    let randomIndex = Math.floor(Math.random() * 9);
+    let randomCol = randomIndex % 3;
+    let randomRow = (randomIndex - randomCol) / 3;
+    while (gameBoard[randomRow][randomCol] !== 0) {
+      randomIndex = Math.floor(Math.random() * 9);
+      randomCol = randomIndex % 3;
+      randomRow = (randomIndex - randomCol) / 3;
+    }
+    gameBoard[randomRow][randomCol] = 1;
+    playerTurn++;
+    updateCell(randomIndex);
   }
 
   // updates grid display
@@ -78,49 +95,69 @@
 
   function announceWinner(value) {
     if (value === 1) {
-      alert("BLUE WON");
+      overlayH2.innerText = "JEDI WON";
+      overlay.style.transform = "scale(1)";
     } else if (value === -1) {
-      alert("RED WON");
+      overlayH2.innerText = "DARTH MAUL WOONNNN";
+      overlay.style.transform = "scale(1)";
     } else {
-      alert("DRAW");
+      overlayH2.innerText = "ITS A DRAWW!";
+      overlay.style.transform = "scale(1)";
     }
     disableGrid();
-    reset();
+    setTimeout(reset, 500);
   }
 
   function checkWinner() {
-    const WINNING_VALUES = [1, -1];
-    for (let value of WINNING_VALUES) {
-      // check rows
-      for (let i = 0; i < 3; i++) {
-        if (gameBoard[i][0] === value && gameBoard[i][1] === value && gameBoard[i][2] === value) {
-          announceWinner(value);
-        }
+    // check rows and columns
+    for (let i = 0; i < 3; i++) {
+      let rowSum = gameBoard[i][0] + gameBoard[i][1] + gameBoard[i][2];
+      let colSum = gameBoard[0][i] + gameBoard[1][i] + gameBoard[2][i];
+      if (rowSum === -3 || colSum === -3) {
+        announceWinner(-1);
+        return;
+      } else if (rowSum === 3 || colSum === 3) {
+        announceWinner(1);
+        return;
       }
+    }
 
-      // check columns
-      for (let j = 0; j < 3; j++) {
-        if (gameBoard[0][j] === value && gameBoard[1][j] === value && gameBoard[2][j] === value) {
-          announceWinner(value);
-        }
-      }
+    // check diagonals
+    let diagonalOneSum = gameBoard[0][0] + gameBoard[1][1] + gameBoard[2][2];
+    let diagonalTwoSum = gameBoard[0][2] + gameBoard[1][1] + gameBoard[2][0];
 
-      // check 2 diagonals
-      if (gameBoard[0][0] === value && gameBoard[1][1] === value && gameBoard[2][2] === value) {
-        announceWinner(value);
-      }
+    if (diagonalOneSum === -3 || diagonalTwoSum === -3) {
+      announceWinner(-1);
+      return;
+    } else if (diagonalOneSum === 3 || diagonalTwoSum === 3) {
+      announceWinner(1);
+      return;
+    }
 
-      if (gameBoard[0][2] === value && gameBoard[1][1] === value && gameBoard[2][0] === value) {
-        announceWinner(value);
-      }
-
-      // check for a draw
-      if (playerTurn === 10) {
-        announceWinner(null);
-      }
+    // check for tie
+    if (gameBoard[0].indexOf(0) === -1 && gameBoard[1].indexOf(0) === -1 && gameBoard[2].indexOf(0) === -1) {
+      announceWinner(null);
+      return;
     }
   }
 
   const resetButton = document.querySelector("[data-reset]");
+  const playerButton = document.querySelector("[data-player]");
+  const aiButton = document.querySelector("[data-ai]");
+  const overlay = document.querySelector("[data-overlay]");
+  const overlayH2 = document.querySelector("[data-overlay-h2]");
   resetButton.addEventListener("click", reset);
+  playerButton.addEventListener("click", () => {
+    if (gameMode === "playerVsPlayer") return;
+    gameMode = "playerVsPlayer";
+    reset();
+  });
+  aiButton.addEventListener("click", () => {
+    if (gameMode === "playerVsAi") return;
+    gameMode = "playerVsAi";
+    reset();
+  });
+  overlay.addEventListener("click", () => {
+    overlay.style.transform = "scale(0)";
+  });
 })();
