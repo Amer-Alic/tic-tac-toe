728db2c666cd7106c7e0c21e76de7a40a4954c4e
amer-tech <alicalan01@gmail.com>
2023-02-05 18:54:49 +0100

    Added minimax algoritham for aiPlay()

--- app.js
+++ app.js
@@ -6,14 +6,12 @@
   ];
 
   let playerTurn = 1;
-  let gameOver = true;
   let gameMode = "playerVsAi";
 
   const gridCells = document.querySelectorAll("[data-cell]");
   gridCells.forEach((gridCell, index) => {
     gridCell.addEventListener("click", () => {
       addMark(index);
-      setTimeout(checkWinner, 200);
     });
   });
 
@@ -36,21 +34,80 @@
       playerTurn++;
       updateCell(index);
     }
+    announceWinner(checkWinner());
   }
 
   function aiPlay() {
+    let bestScore = -Infinity;
+    let bestMove;
+    let index;
+
     if (playerTurn === 10) return;
-    let randomIndex = Math.floor(Math.random() * 9);
-    let randomCol = randomIndex % 3;
-    let randomRow = (randomIndex - randomCol) / 3;
-    while (gameBoard[randomRow][randomCol] !== 0) {
-      randomIndex = Math.floor(Math.random() * 9);
-      randomCol = randomIndex % 3;
-      randomRow = (randomIndex - randomCol) / 3;
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (gameBoard[i][j] === 0) {
+          gameBoard[i][j] = 1;
+          let score = minimax(gameBoard, false);
+          gameBoard[i][j] = 0;
+          if (score > bestScore) {
+            bestScore = score;
+            bestMove = { i, j };
+            // determine which index of cell is gameBoard[i][j] equal to
+            index = i * 3 + j;
+          }
+        }
+      }
     }
-    gameBoard[randomRow][randomCol] = 1;
+    gameBoard[bestMove.i][bestMove.j] = 1;
     playerTurn++;
-    updateCell(randomIndex);
+    updateCell(index);
+    announceWinner(checkWinner());
+  }
+
+  const scores = {
+    X: -1,
+    O: 1,
+    tie: 0,
+  };
+
+  function minimax(gameBoard, isMax) {
+    let result = checkWinner();
+    if (result !== undefined) {
+      return scores[result];
+    }
+
+    if (isMax) {
+      let bestScore = -Infinity;
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          if (gameBoard[i][j] === 0) {
+            gameBoard[i][j] = 1;
+            let score = minimax(gameBoard, false);
+            gameBoard[i][j] = 0;
+            if (score > bestScore) {
+              bestScore = score;
+            }
+          }
+        }
+      }
+      return bestScore;
+    } else {
+      let bestScore = Infinity;
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          if (gameBoard[i][j] === 0) {
+            gameBoard[i][j] = -1;
+            let score = minimax(gameBoard, true);
+            gameBoard[i][j] = 0;
+            if (score < bestScore) {
+              bestScore = score;
+            }
+          }
+        }
+      }
+      return bestScore;
+    }
   }
 
   // updates grid display
@@ -94,15 +151,17 @@
   }
 
   function announceWinner(value) {
-    if (value === 1) {
+    if (value === "O") {
       overlayH2.innerText = "JEDI WON";
       overlay.style.transform = "scale(1)";
-    } else if (value === -1) {
+    } else if (value === "X") {
       overlayH2.innerText = "DARTH MAUL WOONNNN";
       overlay.style.transform = "scale(1)";
-    } else {
+    } else if (value === "tie") {
       overlayH2.innerText = "ITS A DRAWW!";
       overlay.style.transform = "scale(1)";
+    } else {
+      return;
     }
     disableGrid();
     setTimeout(reset, 500);
@@ -114,11 +173,11 @@
       let rowSum = gameBoard[i][0] + gameBoard[i][1] + gameBoard[i][2];
       let colSum = gameBoard[0][i] + gameBoard[1][i] + gameBoard[2][i];
       if (rowSum === -3 || colSum === -3) {
-        announceWinner(-1);
-        return;
+        // announceWinner(-1);
+        return "X";
       } else if (rowSum === 3 || colSum === 3) {
-        announceWinner(1);
-        return;
+        // announceWinner(1);
+        return "O";
       }
     }
 
@@ -127,17 +186,17 @@
     let diagonalTwoSum = gameBoard[0][2] + gameBoard[1][1] + gameBoard[2][0];
 
     if (diagonalOneSum === -3 || diagonalTwoSum === -3) {
-      announceWinner(-1);
-      return;
+      // announceWinner(-1);
+      return "X";
     } else if (diagonalOneSum === 3 || diagonalTwoSum === 3) {
-      announceWinner(1);
-      return;
+      // announceWinner(1);
+      return "O";
     }
 
     // check for tie
     if (gameBoard[0].indexOf(0) === -1 && gameBoard[1].indexOf(0) === -1 && gameBoard[2].indexOf(0) === -1) {
-      announceWinner(null);
-      return;
+      // announceWinner(null);
+      return "tie";
     }
   }
 
